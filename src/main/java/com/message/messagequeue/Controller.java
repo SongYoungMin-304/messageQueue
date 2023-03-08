6892ebcaccf32141a675e6511898cdd93011c566
송여민 <dudals789@naver.com>
2023-03-08 17:38:25 +0900

    kafka consumer 처리

--- src/main/java/com/message/messagequeue/Controller.java
+++ src/main/java/com/message/messagequeue/Controller.java
@@ -1,8 +1,10 @@
 package com.message.messagequeue;
 
 import com.message.messagequeue.ActiveMq.Sender;
+import com.message.messagequeue.Kafka.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +13,20 @@ public class Controller {
     @Autowired
     Sender sender;
 
+    @Autowired
+    KafkaProducer producer;
+
     @GetMapping("/send")
     public String sendMethod() throws Exception{
         sender.send("test");
         return "test";
     }
+
+    @GetMapping("/kafkaSend")
+    public String sendMessage(@RequestParam("message") String message){
+        producer.sendMessage(message);
+        return "success";
+    }
+
 
 }
